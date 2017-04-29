08cb89914ce76d1198aec77a98653a420004e87b
nandrez <nandrez@noreply.github.com>
2017-04-29 21:34:07 +0200

    Add LightComponent

---
+++ app/src/main/java/com/nandrez/hueman/view/components/LightComponent.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2017. nandrez labs, All Rights Reserved
+ */
+package com.nandrez.hueman.view.components;
+
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.util.AttributeSet;
+
+public class LightComponent extends ConstraintLayout {
+    
+    public LightComponent(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+    
+}
