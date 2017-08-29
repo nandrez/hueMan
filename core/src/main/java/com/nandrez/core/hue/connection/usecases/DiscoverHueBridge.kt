41b4e82667b35b96789212be09acf7a717a6d960
nandrez <nandrez@noreply.github.com>
2017-08-29 23:32:42 +0200

    Add DiscoverHueBridge

---
+++ core/src/main/java/com/nandrez/core/hue/connection/usecases/DiscoverHueBridge.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2017. nandrez labs, All Rights Reserved
+ */
+package com.nandrez.core.hue.connection.usecases
+
+import com.nandrez.core.hue.connection.AccessPoint
+import com.nandrez.core.hue.connection.HueConnector
+import com.nandrez.core.hue.connection.HueService
+import com.nandrez.core.lib.UseCase
+
+class DiscoverHueBridge internal constructor(val hueConnector: HueService)
+    : UseCase<DiscoverHueBridge.Request, DiscoverHueBridge.Response>() {
+    
+    constructor() : this(HueConnector())
+    
+    override fun executeUseCase(request: Request, callback: Callback<Response>) {
+        hueConnector.discoverBridge(request.deviceName, object : HueService.DiscoverHueBridgeCallback {
+            
+            override fun onAccessPointsDiscovered(accessPoints: List<AccessPoint>) {
+                val response = Response(accessPoints)
+                callback.onSuccess(response)
+            }
+    
+            override fun onFailedDiscovery() {
+                callback.onError()
+            }
+            
+        })
+    }
+    
+    data class Request(val deviceName: String) : UseCase.Request
+    
+    data class Response(val accessPoint: List<AccessPoint>) : UseCase.Response
+    
+}
+
+
+
