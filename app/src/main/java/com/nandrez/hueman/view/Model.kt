fd13db48178dd3ab413ab7c5e3d8d5efab51079b
nandrez <nandrez@users.noreply.github.com>
2018-04-21 21:27:31 +0200

    Add Model file
    - intended as a container of data-classes

---
+++ app/src/main/java/com/nandrez/hueman/view/Model.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2018. nandrez labs, All Rights Reserved
+ */
+package com.nandrez.hueman.view
+
+data class Bridge(
+        val id: String,
+        val name: String,
+        val ipAddress: String,
+        val macAddress: String,
+        val status: String
+)
